4781a4269aed83b1b955ffb1a254d1ececb6de4d
Guilherme A. Melo <guilhermearmelo@gmail.com>
2023-04-21 23:12:11 -0300

    [FEAT]: Chess board (#2)
    * fix: path naming with lowercase
    
    * feat: Chess board implementation

---
+++ src/main/kotlin/com/guilhermearmelo/fianchetto/domain/entity/Board.kt
@@ -0,0 +1,31 @@
+package com.guilhermearmelo.fianchetto.domain.entity
+
+import com.guilhermearmelo.fianchetto.domain.enums.File
+import com.guilhermearmelo.fianchetto.domain.enums.Rank
+
+class Board {
+
+    private val positions = Array(8) { arrayOfNulls<Square>(8) }
+
+    init {
+        for(i in 1..8) {
+            for(j in 1..8) {
+                positions[i-1][j-1] = Square(File.fromId(i), Rank.fromId(j))
+            }
+        }
+    }
+
+    fun getPositions(): Array<Array<Square?>> {
+        return positions
+    }
+
+    fun showPositions() {
+        for(i in 7 downTo 0) {
+            for(j in 0..7) {
+                print(positions[i][j]!!.getSquareName() + " ")
+            }
+            println()
+        }
+    }
+
+}
---
+++ src/main/kotlin/com/guilhermearmelo/fianchetto/domain/entity/Square.kt
@@ -0,0 +1,13 @@
+package com.guilhermearmelo.fianchetto.domain.entity
+
+import com.guilhermearmelo.fianchetto.domain.enums.File
+import com.guilhermearmelo.fianchetto.domain.enums.Rank
+
+data class Square(
+    var file: File,
+    var rank: Rank
+) {
+    fun getSquareName() : String {
+        return rank.name + (file.ordinal+1).toString()
+    }
+}
