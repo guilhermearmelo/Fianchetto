d11979b37f52c1d9495e95ab15e0b660c8bf9877
Guilherme A. Melo <guilhermearmelo@gmail.com>
2023-04-29 02:01:15 -0300

    refactor: minor classes refactor

--- src/main/kotlin/com/guilhermearmelo/fianchetto/domain/entity/Piece.kt
+++ src/main/kotlin/com/guilhermearmelo/fianchetto/domain/entity/Piece.kt
@@ -1,10 +1,5 @@
 package com.guilhermearmelo.fianchetto.domain.entity
 
-import com.guilhermearmelo.fianchetto.domain.enums.Color
-
-abstract class Piece(
-    private var position: Square,
-    private var color: Color
-) {
-    abstract fun posibleMovements(): List<Square>
+abstract class Piece() {
+    abstract fun possibleMovements()
 }
--- src/main/kotlin/com/guilhermearmelo/fianchetto/domain/entity/Square.kt
+++ src/main/kotlin/com/guilhermearmelo/fianchetto/domain/entity/Square.kt
@@ -4,8 +4,8 @@ import com.guilhermearmelo.fianchetto.domain.enums.File
 import com.guilhermearmelo.fianchetto.domain.enums.Rank
 
 data class Square(
-    var file: File,
-    var rank: Rank
+    var rank: Rank,
+    var file: File
 ) {
     fun getSquareName(): String {
         return rank.name + (file.ordinal + 1).toString()
